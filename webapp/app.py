736542933cb43814a2b4074d73332e9bf178d54a
alejo0998 <alejobusso09@gmail.com>
2022-10-22 02:48:42 -0300

    Otra prueba

--- webapp/app.py
+++ webapp/app.py
@@ -225,6 +225,6 @@ def send_video():
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
