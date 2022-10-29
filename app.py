3c5a5ed70612e040869ece10259abf53826bf39b
alejo0998 <alejobusso09@gmail.com>
2022-10-29 19:39:52 -0300

    Prueba cambio puerto

--- app.py
+++ app.py
@@ -230,7 +230,6 @@ def send_video():
     return respuesta
     
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
 
 
